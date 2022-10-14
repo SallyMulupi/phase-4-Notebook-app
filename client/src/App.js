efd7c9555e10ca83badaf4b424878177e677e8ad
SallyMulupi <mulupisally2gmail.com>
2022-10-14 07:11:23 +0300

    generate classname appp and update it

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,45 @@ function App() {
         localStorage.setItem("notes", JSON.stringify(notes));
     }, [notes]);
     const onAddNote = () => {
-            const newNote = {
-                // id: uuid(),
-                title: "Untitled Note",
-                body: "",
-                lastModified: Date.now(),
-            };
+        const newNote = {
+            // id: uuid(),
+            title: "Untitled Note",
+            body: "",
+            lastModified: Date.now(),
+        };
+        setNotes([newNote, ...notes]);
+        setActiveNote(newNote.id);
+    };
+
+    const onDeleteNote = (noteId) => {
+        setNotes(notes.filter(({ id }) => id !== noteId));
+    };
+    const onUpdateNote = (updatedNote) => {
+        const updatedNotesArr = notes.map((note) => {
+            if (note.id === updatedNote.id) {
+                return updatedNote;
+            }
+            return note;
+        });
+
+        setNotes(updatedNotesArr);
+    };
+
+    const getActiveNote = () => {
+        return notes.find(({ id }) => id === activeNote);
+    };
+    return (
+      <div className="App">
+        <Sidebar
+          notes={notes}
+          onAddNote={onAddNote}
+          onDeleteNote={onDeleteNote}
+          activeNote={activeNote}
+          setActiveNote={setActiveNote}
+        />
+        <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+      </div>
+    );
+  }
+  
+  export default App;
