4fd9bce8cdd632a0e8226400505f4354436640fd
SallyMulupi <mulupisally2gmail.com>
2022-10-14 07:07:56 +0300

    generate app sidebar notes and style it

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -14,3 +14,30 @@ const Sidebar = ({
                 <h1>Notes</h1>
                 <button onClick={onAddNote}>Add</button>
               </div>
+
+              <div className="app-sidebar-notes">
+        {sortedNotes.map(({ id, title, body, lastModified }, i) => (
+          <div
+            className={`app-sidebar-note ${id === activeNote && "active"}`}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title}</strong>
+              <button onClick={(e) => onDeleteNote(id)}>Delete</button>
+            </div>
+            <p>{body && body.substr(0, 100) + "..."}</p>
+            <small className="note-meta">
+              Last Modified{" "}
+              {new Date(lastModified).toLocaleDateString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
